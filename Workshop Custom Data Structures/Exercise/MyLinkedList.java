6fea8c4000743cbd2a258b94b4a14cc39e27370a
Doichin251002 <dodi_hristov@abv.bg>
2024-01-09 23:38:54 +0200

    Problem solutions in Workshop: Data Structures

--- Workshop Custom Data Structures/Exercise/MyLinkedList.java
+++ Workshop Custom Data Structures/Exercise/MyLinkedList.java
@@ -1,9 +1,31 @@
 public class MyLinkedList {
+    private class Node {
+        private int value;
+        private Node next;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
     private Node head;
     private int size;
 
     public void addFirst(int number) {
-        this.head = new Node(number);
+        Node newNode = new Node(number);
+
+        if (!isEmpty()) {
+            newNode.next = this.head;
+        }
+
+        this.head = newNode;
         size++;
+    }
+
+    public int size() {
+        return this.size;
+    }
+
+    public boolean isEmpty() {
+        return this.size == 0;
     }
 }
