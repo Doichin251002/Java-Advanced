f89a6e6ee95ca850823a5593247c6da06205575e
Doichin251002 <dodi_hristov@abv.bg>
2024-01-15 00:11:48 +0200

    Problem solutions in Theme: Generics

--- 7. Generics/Exercise/Sorter.java
+++ 7. Generics/Exercise/Sorter.java
@@ -1,5 +1,13 @@
 public class Sorter {
-    public static void sort(CustomList<T> elements) {
+    public static <T extends Comparable<T>> void sort(CustomList<T> list) {
+        for (int i = 0; i < list.size(); i++) {
+            T current = list.get(i);
 
+            for (int j = i + 1; j < list.size(); j++) {
+                if (current.compareTo(list.get(j)) > 0) {
+                    list.swap(i, j);
+                }
+            }
+        }
     }
 }
