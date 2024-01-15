88cfd4d8d6dbab2638a5eba5e5ac4eb9c69e6bfa
Doichin251002 <dodi_hristov@abv.bg>
2024-01-15 23:19:27 +0200

    Problem solutions in Theme: Iterators and Comparators

--- 8. Iterators and Comparators/Lab/Book.java
+++ 8. Iterators and Comparators/Lab/Book.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 import java.util.List;
 
-public class Book {
+public class Book implements Comparable<Book> {
     private String title;
     private int year;
     private List<String> authors;
@@ -10,6 +10,17 @@ public class Book {
         setTitle(title);
         setYear(year);
         setAuthors(authors);
+    }
+
+    @Override
+    public int compareTo(Book other) {
+        int result = this.title.compareTo(other.title);
+
+        if (result == 0) {
+            result = Integer.compare(this.year, other.year);
+        }
+
+        return result;
     }
 
     public String getTitle() {
