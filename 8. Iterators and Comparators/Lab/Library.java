12590fd3b807e28f608405fc30d6657b053b6198
Doichin251002 <dodi_hristov@abv.bg>
2024-01-15 22:55:58 +0200

    Problem solutions in Theme: Iterators and Comparators

--- 8. Iterators and Comparators/Lab/Library.java
+++ 8. Iterators and Comparators/Lab/Library.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Iterator;
 
 public class Library implements Iterable<Book>{
@@ -9,18 +10,6 @@ public class Library implements Iterable<Book>{
 
     @Override
     public Iterator<Book> iterator() {
-        return new Iterator<Book>() {
-            private int counter = 0;
-
-            @Override
-            public boolean hasNext() {
-                return counter < books.length;
-            }
-
-            @Override
-            public Book next() {
-                return books[counter++];
-            }
-        };
+        return Arrays.stream(books).iterator();
     }
 }
